99b15facc0dcbe02895abc184762219b2346ad01
bytecode014 <olebogengmotsumi97@gmail.com>
2023-02-21 03:12:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exactly what is typed puts funtions
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
