31122c634583f339ac79a53930383b1063f1e72c
bytecode014 <olebogengmotsumi97@gmail.com>
2023-03-21 04:21:46 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,10 @@
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  * Return: always 0
+ *
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {         
 	char letter;
 
