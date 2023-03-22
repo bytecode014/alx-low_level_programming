6ad67353949bce2bdb5f415f7c3000d3d7890b8e
bytecode014 <olebogengmotsumi97@gmail.com>
2023-03-22 07:09:52 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  a function that prints the last digit of a number
+ *
+ * @w: parameter to be checked 
+ *
+ * Return: q
+ */
+
+int print_last_digit(int w)
+{
+	int q;
+
+	q = w % 10;
+	if (w < 0)
+		q = -q;
+	return (w);
+}
