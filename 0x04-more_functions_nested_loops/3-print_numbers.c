6b26c017a11e18ec1bc95b46a878372df8366f98
bytecode014 <olebogengmotsumi97@gmail.com>
2023-03-29 05:05:40 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers -  a function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10)
+		c++;
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
