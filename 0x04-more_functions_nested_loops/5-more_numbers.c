ff433eb07c77c8cb05b2c662f7cdf719e0cc5eb9
bytecode014 <olebogengmotsumi9@gmail.com>
2023-08-01 09:13:36 -0700

    ./6-lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,11 @@
-#include "main.h"
+#inlcude "main.h"
 
 /**
- * more_numbers - print numbers from 0- 14 10 times
- * Return: void
+ * more_numbers - print more numbers
  *
  */
 
 void more_numbers(void)
 {
 	int i, j;
-	for (i = 1; i <= 10; i++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar (1);
-			_putchar (j % 10 + '0');
-		}
-		_putchar ('\n');
-	}
-}
+	for
