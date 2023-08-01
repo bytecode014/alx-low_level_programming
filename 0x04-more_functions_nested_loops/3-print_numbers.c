63d09d5f19c04a0e06c94f6ba1a843e6cbd45e44
bytecode014 <olebogengmotsumi9@gmail.com>
2023-08-01 05:50:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers -  a function that prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers -  a function that prints the numbers from 0 to 9
  *
  * Return: Always 0
  */
 
 void print_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = 0; c < 10)
-		c++;
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - print numbers from 0- 14 10 times
+ * Return: void
  *
  */
 
@@ -10,12 +11,12 @@ void more_numbers(void)
 	int i, j;
 	for (i = 1; i <= 10; i++)
 	{
-	       for (j = 0; j <= 14; j++)
-	       {
-		       if (j >= 10)
-			       _putchar('1');
-		       _putchar (j % 10 + '0');
-	       }
-	       _putchar ('/n');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar (1);
+			_putchar (j % 10 + '0');
+		}
+		_putchar ('\n');
 	}
-}	       
+}
