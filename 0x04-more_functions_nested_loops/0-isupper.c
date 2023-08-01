ce09fd685e17b82c6c8ffd2a650d52fc164487c4
bytecode014 <olebogengmotsumi9@gmail.com>
2023-08-01 04:51:29 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters 
- * @c: check char
- *
- * Return : 0 or 1
+ * _isupper - lowercase letters 
+ * @c:check character
+ * Return: 1 for uppercase character or 0 for anything else
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && <= 'Z')
-		return(1);
+	if (c >= 65 && <= 90)
+		return (1);
 	else
 		return (0);
 }
---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#inlude "main.h
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
